750576c953076832307084bbff435688c9ed6abd
Julian Schwarz <j.schwarz@pixelart.at>
2018-07-30 16:30:49 +0200

    adapt max player amount

--- assets/js/modules/racer.js
+++ assets/js/modules/racer.js
@@ -56,7 +56,7 @@ App.Racer = (function (undefined) {
                 0
             ],
             DRONE_OBJECT: '/public/objects/Drone_05.dae',
-            MAX_AMOUNT: 1,
+            MAX_AMOUNT: 2,
             SIZE: 7,
             TURN_TIME: 10,
             TURN_SCALE_FACTOR: 0.08,
