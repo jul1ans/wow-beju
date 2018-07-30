26fbba3ee58c932b4b94ecc91a3726f55a873c23
Julian Schwarz <j.schwarz@pixelart.at>
2018-07-30 15:23:07 +0200

    set correct max amount

--- assets/js/modules/racer.js
+++ assets/js/modules/racer.js
@@ -56,7 +56,7 @@ App.Racer = (function (undefined) {
                 0
             ],
             DRONE_OBJECT: '/public/objects/drone.dae',
-            MAX_AMOUNT: 1,
+            MAX_AMOUNT: 2,
             SIZE: 7,
             TURN_TIME: 10,
             TURN_SCALE_FACTOR: 0.08,
